13ac5f5e0367382e416b0162112be42ef11422f2
shostkotimofei@gmail.com <Timohgako1814>
2020-02-06 16:09:04 +0300

    Added repository interfaces

---
+++ src/main/java/com/leverx/odata2train/repository/CatRepositoryImpl.java
@@ -0,0 +1,28 @@
+package com.leverx.odata2train.repository;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static java.util.Collections.emptyMap;
+
+public class CatRepositoryImpl implements CatRepository {
+
+    public Map<String, Object> getById(long id) {
+        if (id == 1) {
+            HashMap<String, Object> cat = new HashMap<>();
+            cat.put("Id", 1);
+            cat.put("Name", "Steve");
+            cat.put("Owner", null);
+            return cat;
+        }
+        return emptyMap();
+    }
+
+    public List<Map<String, Object>> getAll() {
+        final ArrayList<Map<String, Object>> objects = new ArrayList<>();
+        objects.add(getById(1));
+        return objects;
+    }
+}
--- src/main/java/com/leverx/odata2train/repository/UserRepository.java
+++ src/main/java/com/leverx/odata2train/repository/UserRepository.java
@@ -1,29 +1,11 @@
 package com.leverx.odata2train.repository;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import static java.util.Collections.emptyList;
-import static java.util.Collections.emptyMap;
+public interface UserRepository {
 
-public class UserRepository {
+    Map<String, Object> getById(long id);
 
-    public Map<String, Object> getById(long id) {
-        if (id == 1) {
-            HashMap<String, Object> user = new HashMap<>();
-            user.put("Id", 1);
-            user.put("Name", "Steve");
-            user.put("Cats", emptyList());
-            return user;
-        }
-        return emptyMap();
-    }
-
-    public List<Map<String, Object>> getAll() {
-        final ArrayList<Map<String, Object>> objects = new ArrayList<>();
-        objects.add(getById(1));
-        return objects;
-    }
+    List<Map<String, Object>> getAll();
 }
